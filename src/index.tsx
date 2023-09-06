c2c0871ab3672d3087d196a3171a0730d7ad38e2
Krish120003 <krish120003@gmail.com>
2023-09-06 17:18:23 -0400

    feat: scaffold list

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,37 @@
-import { Detail } from "@raycast/api";
+import { List } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import GtfsRealtimeBindings from "gtfs-realtime-bindings";
 
 export default function Command() {
-  return <Detail markdown="# Hello World" />;
+  const { isLoading, data, revalidate, error } = useFetch("https://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb");
+
+  if (isLoading) return <List isLoading={true} />;
+
+  if (error) {
+    return (
+      <List>
+        <List.Item title="Error" subtitle={error.message} />
+      </List>
+    );
+  }
+
+  // parse data into GTFS-Realtime format
+  console.log(data);
+  // print the data type of the data
+  console.log(typeof data);
+  const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(data.arrayBuffer));
+
+  feed.entity.forEach((entity) => {
+    if (entity.tripUpdate) {
+      console.log(entity.tripUpdate);
+    }
+  });
+
+  return (
+    <List>
+      <List.Item title="Item 1" />
+      <List.Item title="Item 1" />
+      <List.Item title="Item 1" />
+    </List>
+  );
 }
